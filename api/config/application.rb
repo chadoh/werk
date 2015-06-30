9d7b9968e8c05ec5eb08dfee3ab4784bb77fa245
Chad Ostrowski <hi@chadoh.com>
2015-06-30 09:09:38 -0400

    Move cors config to initializer

--- api/config/application.rb
+++ api/config/application.rb
@@ -22,14 +22,5 @@ module Api
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-
-    config.middleware.insert_before 'Rack::Runtime', 'Rack::Cors' do
-      allow do
-        origins '*'
-        resource '*',
-                 headers: :any,
-                 methods: [:get, :put, :post, :patch, :delete, :options]
-      end
-    end
   end
 end
---
+++ api/config/initializers/cors.rb
@@ -0,0 +1,8 @@
+Rails.application.config.middleware.insert_before 'Rack::Runtime', 'Rack::Cors' do
+  allow do
+    origins '*'
+    resource '*',
+             headers: :any,
+             methods: [:get, :put, :post, :patch, :delete, :options]
+  end
+end
